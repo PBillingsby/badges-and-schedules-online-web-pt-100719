3643174dcb84f9cd5b22a2845d3e4657911e3b67
Phil Billingsby <paulbillingsby@gmail.com>
2020-01-18 23:32:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -5,4 +6,9 @@ end
 
 def batch_badge_creator(arr)
   arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(names)
+  count = 0
+  binding.pry
 end
