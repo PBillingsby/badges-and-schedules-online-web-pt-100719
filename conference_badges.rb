5dd2d6c5a3359a7f0343c8c0becadd3559b3073f
Phil Billingsby <paulbillingsby@gmail.com>
2020-01-18 23:29:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,7 @@
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(hash)
+  hash.each {|name| "Hello, my name is #{name}.}
